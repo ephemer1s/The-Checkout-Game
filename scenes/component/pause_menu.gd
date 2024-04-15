4e938310ff54d16cfaf278d414ea14b989c421af
Spica TSUKIMI <haochengvanadis@gmail.com>
2024-04-15 16:17:23 -0400

    Add audio nodes, create audio for main menu and pause menu buttons

--- scenes/component/pause_menu.gd
+++ scenes/component/pause_menu.gd
@@ -10,16 +10,21 @@ func _unhandled_input(event):
     if event.is_action_pressed("pause"):
         #get_tree().paused = !get_tree().paused
         #visible = !visible
+        Audio.get_node("sfx_button_click").play()
         self.is_paused = !is_paused
 
 func _on_resume_pressed():
     print("resume")
     #get_tree().paused = false
     #visible = false
+    Audio.get_node("sfx_button_click").play()
+    
     self.is_paused = false
     
 func _on_main_menu_pressed():
     print("return to menu")
+    Audio.get_node("sfx_button_click").play()
+    
     get_tree().paused = false
     get_tree().change_scene_to_file("res://scenes/main/main-menu.tscn")
 
@@ -31,3 +36,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+
+
+func _on_resume_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
+    pass # Replace with function body.
+
+
+func _on_main_menu_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
+    pass # Replace with function body.
--- scenes/main/main-menu.gd
+++ scenes/main/main-menu.gd
@@ -18,24 +18,49 @@ func _process(delta):
     pass
 
 
+#func start_game():
+    #get_tree().change_scene_to_packed(game)
+    
+
 func _on_btn_new_game_pressed():
     if OS.is_debug_build():
         print("[DEBUG MSG] Button <New Game> Pressed!")
+    Audio.get_node("sfx_button_click").play()
+    #call_deferred("start_game")
     get_tree().change_scene_to_packed(game)
     pass
 
 
 func _on_btn_options_pressed():
     var scn_options = load("res://scenes/main/options.tscn").instantiate()
+    Audio.get_node("sfx_button_click").play()
     get_tree().current_scene.add_child(scn_options)
     pass
 
 
 func _on_btn_credit_pressed():
     var scn_credits = load("res://scenes/main/credits.tscn").instantiate()
+    Audio.get_node("sfx_button_click").play()
     get_tree().current_scene.add_child(scn_credits)
     pass
 
 
 func _on_btn_quit_game_pressed():
+    Audio.get_node("sfx_button_click").play()
     get_tree().quit()
+
+
+func _on_btn_new_game_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
+
+
+func _on_btn_options_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
+
+
+func _on_btn_credits_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
+
+
+func _on_btn_quit_game_mouse_entered():
+    Audio.get_node("sfx_button_highlight").play()
