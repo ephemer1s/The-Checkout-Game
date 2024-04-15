f329a89aca2e418fee38237af853f9ac53897468
mizuto <billhe1989@gmail.com>
2024-04-15 11:35:12 -0700

    option color and demonic font

--- scenes/component/shadow_button.gd
+++ scenes/component/shadow_button.gd
@@ -11,6 +11,11 @@ var is_hovering = false
 var next_state_id
 var money_action = ""
 
+var demonic = preload("res://assets/fonts/Demon-Regular.ttf")
+var normal = preload("res://assets/fonts/SueEllenFrancisco-Regular.ttf")
+
+var demonic_color = Color(1, 0.78, 0)
+var normal_color = Color(0, 0, 0)
 enum State {
     READY,
     HOVER,
@@ -38,6 +43,23 @@ func on_mouse_exited() -> void:
         cur_state = State.READY
 
 func setup_button(text: String, _next_state_id: int, _money_action: String) -> void:
+    if text[0] == "[":
+        button.add_theme_font_override("font", demonic)
+        button.add_theme_color_override("font_color", demonic_color)
+        button.add_theme_color_override("font_hover_color", demonic_color)
+        button.add_theme_color_override("font_hover_pressed_color", demonic_color)
+        button.add_theme_color_override("font_pressed_color", demonic_color)
+        button.add_theme_color_override("font_focus_color", demonic_color)
+        shadow.add_theme_font_override("font", demonic)
+    else:
+        button.add_theme_font_override("font", normal)
+        button.add_theme_color_override("font_color", normal_color)
+        button.add_theme_color_override("font_hover_color", normal_color)
+        button.add_theme_color_override("font_hover_pressed_color", normal_color)
+        button.add_theme_color_override("font_pressed_color", normal_color)
+        button.add_theme_color_override("font_focus_color", normal_color)
+        shadow.add_theme_font_override("font", normal)
+    text = text.replace("[", "").replace("]", "")
     button.text = text
     shadow.text = text
     next_state_id = _next_state_id
