f7d1374ec9ba21d424100392551250e231364406
mizuto <billhe1989@gmail.com>
2024-04-15 10:47:20 -0700

    achievement

--- scenes/component/end_screen.gd
+++ scenes/component/end_screen.gd
@@ -2,14 +2,19 @@ extends CanvasLayer
 
 @onready var end = $End
 @onready var true_end = $TrueEnd
+@onready var animation_player = $AnimationPlayer
+@onready var achievement_box = $AchievementBox
 
-# Called when the node enters the scene tree for the first time.
+var menu: PackedScene = preload("res://scenes/main/main-menu.tscn")
+var can_quit = false
+
 func _ready() -> void:
     end.visible = false
     true_end.visible = false
+    play_end("soulless")
 
 func play_end(end_name: String) -> void:
-    print("END NAME:", end_name)
+    achievement_box.set_achievement(end_name)
     if end_name == "summoning":
         true_end.visible = true
         true_end.play("default")
@@ -18,3 +23,14 @@ func play_end(end_name: String) -> void:
     else:
         end.visible = true
         end.play(end_name)
+    animation_player.play("show_achivement")
+    await get_tree().create_timer(4).timeout
+    animation_player.play("hide_achivement")
+    await animation_player.animation_finished
+    can_quit = true
+
+func _process(delta: float) -> void:
+    if can_quit and (Input.is_action_just_pressed("confirm") || Input.is_action_just_pressed("keyboard_confirm")) || Input.is_action_just_pressed("esc"):
+        animation_player.play("fade_out")
+        await animation_player.animation_finished
+        get_tree().change_scene_to_packed(menu)
