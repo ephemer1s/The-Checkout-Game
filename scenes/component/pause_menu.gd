845f861b54e1ec9a7463ad4995f134c0c2ec0792
Spica TSUKIMI <haochengvanadis@gmail.com>
2024-04-15 02:30:57 -0400

    pause menu

---
+++ scenes/component/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var is_paused = false:
+    set(value):
+        is_paused = value
+        get_tree().paused = is_paused
+        visible = is_paused
+    
+func _unhandled_input(event):
+    if event.is_action_pressed("pause"):
+        #get_tree().paused = !get_tree().paused
+        #visible = !visible
+        self.is_paused = !is_paused
+
+func _on_resume_pressed():
+    print("resume")
+    #get_tree().paused = false
+    #visible = false
+    self.is_paused = false
+    
+func _on_main_menu_pressed():
+    print("return to menu")
+    get_tree().paused = false
+    get_tree().change_scene_to_file("res://scenes/main/main-menu.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
